0435c987700319b9e39f6878250855f0a98840eb
Naresh Prajapati <prajapatinaresh2152006@gmail.com>
2025-08-31 23:50:55 +0530

    Initial commit

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -56,7 +56,7 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1.2 }}
-          href="/sample-resume.pdf"
+          href="/NARESH_PRAJAPATI_RESUME_LJIET.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
         >
